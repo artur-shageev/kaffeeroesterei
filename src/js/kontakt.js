186e08a5f29d86f063571354a58fc8aa851f1810
artur <frontend.shageev@gmail.com>
2021-02-05 19:30:15 +0100

    feedback created

---
+++ src/js/kontakt.js
@@ -0,0 +1,19 @@
+const header = document.querySelector('.container')
+header.classList.add('dark')
+
+const selector = document.querySelector('.formular-selector__placeholder')
+const menu = document.querySelector('.formular-selector__menu')
+
+selector.addEventListener('click', ()=>{
+    // menu.classList.forEach((el)=>{
+    //     if (el === 'formular-selector__menu-open') {
+    //         menu.classList.remove('formular-selector__menu-open')
+    //     }else{
+    //         menu.classList.add('formular-selector__menu-open')
+    //     }
+    // }
+    menu.classList.toggle('formular-selector__menu-open')
+    console.log(menu);
+    menu.style.height = menu.offsetHeight 
+})
+
